91f740f6de741e019a8c50ff41cc4acb869662f7
Bice93 <bianca.cerulo@hotmail.it>
2023-01-27 17:59:35 +0100

    Fix on the class Bank

--- src/jana60/bank/Bank.java
+++ src/jana60/bank/Bank.java
@@ -17,20 +17,37 @@ public class Bank {
 		int accountNumber = r.nextInt(999)+1;
 		
 		newConto = new Conto(name, accountNumber);
-		//System.out.println(newConto.name + " - "  + newConto.accountNumber);
-		System.out.println("Che tipo di operazione vuoi svolgere?");
-		System.out.println("* Versamento");
-		System.out.println("* Prelievo");
-		System.out.println("* Esci");
 		
-		String choise = s.nextLine();
-
-		if (choise.equals("Versamento")) {
-			System.out.println("Quanto vuoi versare?");
-			int money = Integer.parseInt(s.nextLine());
-			System.out.println("Il tuo saldo attuale è: " + newConto.addMoney(money));
+		String choise = "Versamento";
+		
+		while (!choise.equals("Esci")) {
+			System.out.println("Che tipo di operazione vuoi svolgere?");
+			System.out.println("* Versamento");
+			System.out.println("* Prelievo");
+			System.out.println("* Esci");
+			
+			choise = s.nextLine();
+			
+			if (choise.equals("Versamento")) {
+				System.out.println("Quanto vuoi versare?");
+				int money = Integer.parseInt(s.nextLine());
+				System.out.println("Il tuo saldo attuale è: " + newConto.addMoney(money));
+				System.out.println();
+			} else if (choise.equals("Prelievo")) {
+				System.out.println("Quanto vuoi prelevare?");
+				int money = Integer.parseInt(s.nextLine());
+				if(newConto.isWithdrawal(money)) {
+					System.out.println("Il tuo saldo attuale è: " + newConto.getBalance());	
+					System.out.println();
+				}else {
+					System.out.println("Mi dispiace non puoi prelevare! Il tuo saldo attuale è: " + newConto.getBalance());
+					System.out.println();
+				}
+			} else if (choise.equals("Esci")) {
+				System.out.println("A presto " + newConto.getName() + "!");
+			}
+			
 		}
-		
 		
 		s.close();
 	}
